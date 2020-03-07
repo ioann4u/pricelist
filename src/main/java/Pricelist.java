1b8b6c4fed3d59fa94d1660f5aed05667fb21869
ioann4u <panikerenot@gmail.com>
2020-03-07 19:59:48 +0300

    I've done smth.

--- src/main/java/Pricelist.java
+++ src/main/java/Pricelist.java
@@ -1,10 +1,24 @@
+import java.util.HashMap;
 import java.util.Map;
 import java.util.TreeMap;
 
 public class Pricelist {
-    private int article;
-    private String name;
-    private double cost;
+    //private int article;
+    //private String name;
+    //private double cost;
     //private static TreeMap<> data;
+    private static HashMap<Integer, String> pl = new HashMap<Integer, String>();
 
+    //add - добавляет запись по заданным id и продуктам
+    private static void add(Integer id, String productAndPrice) {
+        pl.put(id, productAndPrice);
+    }
+
+
+    //del - удаляет записи по id
+    private static void del(Integer id) {
+        pl.remove(id);
+    }
+
+    //private static void
 }
